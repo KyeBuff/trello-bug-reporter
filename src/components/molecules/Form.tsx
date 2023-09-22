cd92c7d5f6c7ce7150015e75d305400906e7fce1
Kye Buffery <kyebuffery14@gmail.com>
2023-09-22 16:37:55 +0100

    main: Add fonts

--- src/components/molecules/Form.tsx
+++ src/components/molecules/Form.tsx
@@ -13,6 +13,10 @@ const Form = styled.form`
 
   background-color: #485b9c;
   box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
+
+  button[type="submit"] {
+    margin-top: 1rem;
+  }
 `;
 
 export default Form;
