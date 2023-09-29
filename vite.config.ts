ad5265fe07741dde1d4feebd83115ea8c37406d2
Kye Buffery <kyebuffery14@gmail.com>
2023-09-29 11:53:13 +0100

    main: Allow import as regular JS module

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    lib: {
+      entry: path.resolve("src", "main.tsx"),
+      name: "lunar-bug-tool",
+      fileName: (format) => `lunar-bug-tool.${format}.js`,
+    },
+    rollupOptions: {
+      output: {
+        globals: {
+          react: "React",
+        },
+      },
+    },
+  },
   plugins: [react()],
-})
+  define: {
+    "process.env": {
+      NODE_ENV: "production",
+    },
+  },
+});
