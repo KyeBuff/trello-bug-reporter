a502a6c07be0d6bc0d6c3adba3e6f3a54589b8b5
Kye Buffery <kyebuffery14@gmail.com>
2023-09-29 13:00:26 +0100

    main: Working example of BugTool imported

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 import BugTool from "./dist/lunar-bug-tool.es.js";
 
-console.log(
-  BugTool.init({
-    key: "4a3fb7dc39032a123e911f41fc0c17e3",
-    token:
-      "ATTA9c52ab7e727e26698f5ef71b9c2a1102cf0e598221b773cbfd549376ddc516661169EF54",
-  })
-);
+BugTool.init({
+  key: "4a3fb7dc39032a123e911f41fc0c17e3",
+  token:
+    "ATTA9c52ab7e727e26698f5ef71b9c2a1102cf0e598221b773cbfd549376ddc516661169EF54",
+  boardId: "644a2880404482591ad18e38",
+  listId: "644a28be4d4825d44b85b048",
+});
